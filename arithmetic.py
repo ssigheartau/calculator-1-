6e97511c8b99fa6521c4b735f1cbada0adfae93c
ssigheartau <ssigheartau@yahoo.com>
2024-04-02 20:34:21 -0700

    Added squ, cub, power, mod

--- arithmetic.py
+++ arithmetic.py
@@ -27,15 +27,23 @@ def divide(num1, num2):
 
 def square(num1):
     """Return the square of num1."""
-
+    squ = num1 ** 2
+    return squ
 
 def cube(num1):
     """Return the cube of num1."""
-
+    cub = num1 ** 3
+    return cub
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
-
+    pow = base ** exponent
+    return pow
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    if num2 != 0:
+      mod = num1 % num2
+      return mod
+    else:
+      return "Error: Modulo by zero"
